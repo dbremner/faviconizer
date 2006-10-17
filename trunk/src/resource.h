3a6a9664a1ee67f89ee77f1ea47ecafc2fece807
steveking <steveking@8a0c7f93-05ce-44a3-a273-0a297b55e7ed>
2006-10-17 17:53:35 +0000

    Change the text label in the dialog and set the default button to "find missing icons".

--- trunk/src/resource.h
+++ trunk/src/resource.h
@@ -2,10 +2,8 @@
 // Microsoft Visual C++ generated include file.
 // Used by FavIconizer.rc
 //
-#define IDC_MYICON                      2
 #define IDD_FAVICONIZER_DIALOG          102
 #define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
 #define IDM_EXIT                        105
 #define IDI_FAVICONIZER                 107
