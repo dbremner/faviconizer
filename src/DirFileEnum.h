f275e97cf5ef0099ed551ba7e44348f8cd1653d5
steveking <steveking@8a0c7f93-05ce-44a3-a273-0a297b55e7ed>
2008-03-15 19:11:09 +0000

    Add GPL headers.

--- src/DirFileEnum.h
+++ src/DirFileEnum.h
@@ -1,7 +1,24 @@
+// FavIconizer
+
+// Copyright (C) 2007-2008 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 #pragma once
 
 /**
- * \ingroup Utils
  * Enumerates over a directory tree, non-recursively.
  * Advantages over CFileFind:
  * 1) Return values are not broken.  An error return from
